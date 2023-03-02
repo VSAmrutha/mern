1f210d08a6a610009d58e5c6efae04e8fe4751da
Amrutha-V-S <Amrutha.V.S@ibm.com>
2023-03-02 11:39:05 +0530

    hash, jwt

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,20 @@
-import {StatusCodes} from 'http-status-codes'
+import {StatusCodes} from 'http-status-codes';
+
 const errorHandlerMiddleware=(err,req,res,next)=>{
     console.log(err)
     const defaultError={
-        statusCodes:StatusCodes.INTERNAL_SERVER_ERROR,
-        msg:'Soemthing went wrong try again later'
+        statusCodes:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:err.message || 'Soemthing went wrong try again later'
     }
+
     if(err.name === 'ValidationError'){
     defaultError.statusCodes=StatusCodes.BAD_REQUEST;
     //defaultError.msg=err.message
     defaultError.msg=Object.values(err.errors).map(item=>item.message).join(', ')
+    }
+    if(err.code && err.code===11000){
+        defaultError.statusCodes=StatusCodes.BAD_REQUEST;
+        defaultError.msg=`${Object.keys(err.keyValue)} field has to be unique`
     }
     res.status(defaultError.statusCodes).json({msg:defaultError.msg})
 }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 const UserSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -20,7 +22,9 @@ const UserSchema=new mongoose.Schema({
     password:{
         type:String,
         required:[true, 'Please provide password'],
-        minlength:6 
+        minlength:6,
+        // if we add select:false then it should be showing the passowrd, but this does not work with .create method
+        select:false
     },
     lastName:{
         type:String,
@@ -35,5 +39,13 @@ const UserSchema=new mongoose.Schema({
         default:'city'
     }
 });
+UserSchema.pre('save',async function(){
+    const salt=await bcrypt.genSalt(10)
+    this.password=await bcrypt.hash(this.password,salt);
 
+})
+UserSchema.methods.createJWT=function(){
+    return jwt.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+   
+}
 export default mongoose.model('User',UserSchema);
